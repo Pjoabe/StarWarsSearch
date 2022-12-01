03e8f89e6e4d99a830f3a622bb10db5dd14d050a
joabe <joabe9999pereira@gmail.com>
2022-12-01 17:20:11 -0300

    requisito 3

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from 'react';
 function Table() {
   const [data, setData] = useState([]);
   const [toFilter, setTofilter] = useState('');
+  const [colum, setColum] = useState('population');
+  const [toTreat, setToTreat] = useState('maior que');
+  const [values, setValues] = useState(0);
+  const [numberFilter, setNumberFilter] = useState(null);
 
   const apiFetch = async () => {
     const request = await fetch('https://swapi.dev/api/planets');
@@ -17,6 +21,23 @@ function Table() {
 
   const filtered = data.filter(({ name }) => name.includes(toFilter));
 
+  const treatConditions = (param) => {
+    switch (param) {
+    case 'maior que':
+      setNumberFilter(filtered
+        .filter((el) => (+el[colum] > +values)));
+      break;
+    case 'menor que':
+      setNumberFilter(filtered
+        .filter((el) => (+el[colum] < +values)));
+      break;
+    case 'igual a':
+      setNumberFilter(filtered
+        .filter((el) => (+el[colum] === +values)));
+      break;
+    default:
+    }
+  };
   return (
     <>
       <input
@@ -25,6 +46,39 @@ function Table() {
         value={ toFilter }
         onChange={ ({ target: { value } }) => setTofilter(value) }
       />
+      <select
+        data-testid="column-filter"
+        value={ colum }
+        onChange={ ({ target: { value } }) => setColum(value) }
+      >
+        <option>population</option>
+        <option>orbital_period</option>
+        <option>diameter</option>
+        <option>rotation_period</option>
+        <option>surface_water</option>
+      </select>
+      <select
+        data-testid="comparison-filter"
+        value={ toTreat }
+        onChange={ ({ target: { value } }) => setToTreat(value) }
+      >
+        <option>maior que</option>
+        <option>menor que</option>
+        <option>igual a</option>
+      </select>
+      <input
+        data-testid="value-filter"
+        type="number"
+        value={ values }
+        onChange={ ({ target: { value } }) => setValues(value) }
+      />
+      <button
+        data-testid="button-filter"
+        type="button"
+        onClick={ () => treatConditions(toTreat) }
+      >
+        Filtrar
+      </button>
       <table>
         <tr>
           <th>Name</th>
@@ -42,7 +96,25 @@ function Table() {
           <th>URL</th>
         </tr>
         <tbody>
-          {data.length > 0 && filtered.map((el) => (
+          {data.length > 0 && numberFilter === null
+           && filtered.map((el) => (
+             <tr key={ el.name }>
+               <td>{el.name}</td>
+               <td>{el.rotation_period}</td>
+               <td>{el.orbital_period}</td>
+               <td>{el.diameter}</td>
+               <td>{el.climate}</td>
+               <td>{el.gravity}</td>
+               <td>{el.terrain}</td>
+               <td>{el.surface_water}</td>
+               <td>{el.population}</td>
+               <td>{el.films}</td>
+               <td>{el.created}</td>
+               <td>{el.edited}</td>
+               <td>{el.url}</td>
+             </tr>
+           ))}
+          {numberFilter !== null && numberFilter.map((el) => (
             <tr key={ el.name }>
               <td>{el.name}</td>
               <td>{el.rotation_period}</td>
